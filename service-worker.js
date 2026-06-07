b1f4c04df05dc521c75867661db5e227ce1714e5
raazchoudhary765-droid <raazchoudhary765@gmail.com>
2026-06-08 02:10:58 +0530

    Add fetch event listener for offline response

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,15 @@
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Offline');
+    })
+  );
 });
